9be7a3d79b8aa8dc4de896b58d7971ac77ddb8b2
Boubacar <boubacar.diallo1@macaulay.cuny.edu>
2020-02-29 23:36:14 -0500

    remove folder

---
+++ boubareact/build/precache-manifest.4c1b1d2a60c9f73dfa7b72f7385e3e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ad06d1b8bdd2d04413b9d461492055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b99c9ae20db838e0bb",
+    "url": "/static/css/main.9adb6b7b.chunk.css"
+  },
+  {
+    "revision": "30a283ce1c36f9b15220",
+    "url": "/static/js/2.89393565.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.89393565.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73b99c9ae20db838e0bb",
+    "url": "/static/js/main.48d07c4b.chunk.js"
+  },
+  {
+    "revision": "9cf5796cf22850e383f7",
+    "url": "/static/js/runtime-main.027db947.js"
+  }
+]);

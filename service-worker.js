28756670c6fdc3b0c198ff85e4416f15e3f5d510
Boubacar <boubacar.diallo1@macaulay.cuny.edu>
2020-03-05 12:22:24 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e3b57d2688754f5224b0fa2499dab80.js"
+  "/precache-manifest.210483fa3034f9e239aa9247b2553f31.js"
 );
 
 self.addEventListener('message', (event) => {
